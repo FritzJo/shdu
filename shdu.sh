59cd6fe21428ddd7e19be3cb45d6b1ffcc18dd28
FritzJo <fritzjo-git@mailbox.org>
2020-04-11 12:34:58 +0200

    shellcheck

--- shdu.sh
+++ shdu.sh
@@ -6,11 +6,11 @@ if [ "$path" = "" ]; then
 fi
 
 dirs=$(find $path -maxdepth 1 -mindepth 1 -type d -printf '%f;')
-my_array=($(echo $dirs | tr ";" "\n"))
+my_array=($(echo "$dirs" | tr ";" "\n"))
 
 for i in "${!my_array[@]}"; do
   index=$i
-  disk=$(du -s $path/${my_array[$index]} | cut -d'/' -f1)
+  disk=$(du -s $path/"${my_array[$index]}" | cut -d'/' -f1)
   my_array[$index]="${my_array[$index]} $disk"
 done
 
@@ -18,7 +18,7 @@ IFS=$'\n' sorted=($(sort -k2 -r -n <<<"${my_array[*]}"))
 unset IFS
 
 for i in "${sorted[@]}"; do
-  i=$(echo $i | cut -d' ' -f1)
-  disk=$(du -sh $path/$i | cut -d'/' -f1)
-  printf '%s\t%s\t%s\n' "$i" "$disk"
+  i=$(echo "$i" | cut -d' ' -f1)
+  disk=$(du -sh $path/"$i" | cut -d'/' -f1)
+  printf '%s\t%s\n' "$i" "$disk"
 done
